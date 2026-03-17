1dacf96f237cc67c161af2cadb34d2a1cfe20639
Quoc Cuong <quoccuong14012000@gmail.com>
2026-03-17 21:33:30 +0700

    Update: restruct project

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,11 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '../../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
     const adapter = new PrismaPg({ url: process.env.DATABASE_URL });
     super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
   }
 }
