abf9caab0840cc060fd35b357660a2731c2e1e98
Quoc Cuong <quoccuong14012000@gmail.com>
2026-03-24 11:08:33 +0700

    Update: packages and decorator for auth

---
+++ src/common/decorators/current-user.decorator.ts
@@ -0,0 +1,11 @@
+import { Request } from 'express';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const CurrentUser = createParamDecorator(
+  (data: string | undefined, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest<Request>();
+    const user = request.user as Record<string, unknown> | undefined;
+
+    return data ? user?.[data] : user;
+  },
+);
