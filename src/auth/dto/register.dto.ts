af8d12d9c9dae96ef860983d5b9587bf606f0e37
Quoc Cuong <quoccuong14012000@gmail.com>
2026-03-24 17:39:25 +0700

    Update: DTO for login and register

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,26 @@
+import {
+  IsEmail,
+  IsString,
+  MinLength,
+  MaxLength,
+  Matches,
+} from 'class-validator';
+
+export class RegisterDto {
+  @IsString()
+  @MinLength(3)
+  @MaxLength(30)
+  username: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(72)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/, {
+    message:
+      'Password must contain at least one uppercase letter, one lowercase letter, and one number',
+  })
+  password: string;
+}
